29b25261d5eebb3b72c1e0bdbe2b282bec4b61d1
WonWonGit <bkn020612@gmail.com>
2023-01-04 20:11:46 +0900

    img

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { StarDivLeft, StarDivRight, StyledBannerTitle } from "./StyledCommon";
-import Star from "../images/Star.png";
+import Star from "../images/star.png";
 
 const ProjectWrap = styled.div`
   width: 80%;
